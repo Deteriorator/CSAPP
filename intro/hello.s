b789bd6bf65e477cdd92372b1f036c0bc18d2730
Deteriorator <Liangz.org@gmail.com>
2020-07-13 10:21:54 +0800

    feat(chap1): chap1 code:intro

---
+++ intro/hello.s
@@ -0,0 +1,24 @@
+	.file	"hello.c"
+	.text
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC0:
+	.string	"Hello, World"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB11:
+	.cfi_startproc
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 16
+	leaq	.LC0(%rip), %rdi
+	call	puts@PLT
+	movl	$0, %eax
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE11:
+	.size	main, .-main
+	.ident	"GCC: (Debian 9.3.0-14) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
